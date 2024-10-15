87dd7bcfc8d2f67bc4044f17742b9ce19bc1b8ef
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 04:35:30 -0700

    task 3 completed

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const getUserChoice = (userInput) => {
   userInput = userInput.toLowerCase();
-}
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors" ||
+    userInput === "bomb"
+  ) {
+    return userInput;
+  } else {
+    console.log("User input is an invalid entry.");
+  }
+};
