f2b2128f190b60ea0b6ba1cc12e438d08f2b9917
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 04:40:52 -0700

    completed project

--- app.js
+++ app.js
@@ -27,5 +27,35 @@ let getComputerChoice = () => {
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
     return "It's a tie!";
+  } else if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "You lose, paper covers rock!";
+    } else {
+      return "You win, rock beats scissors!";
+    }
+  } else if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "You lose, scissors cuts paper!";
+    } else {
+      return "You win, paper covers rock!";
+    }
+  } else if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "You lose, rock beats scissors!";
+    } else {
+      return "You win, scissors cut paper!";
+    }
+  } else {
+    return "BOOM! You win.";
   }
+};
+
+function playGame() {
+  let userChoice = getUserChoice("bomb");
+  let computerChoice = getComputerChoice();
+  console.log(userChoice);
+  console.log(computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
 }
+
+playGame();
